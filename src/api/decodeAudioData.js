78631d68e74d799d134711b4ecba45d28652421f
Joe Harlow <joe@f5.io>
2015-06-13 16:10:25 +0100

    Clean up on aisle 3

--- src/api/decodeAudioData.js
+++ src/api/decodeAudioData.js
@@ -1,7 +1,7 @@
 export default function decodeAudioData(arrayBuffer) {
 	let context = new AudioContext();
 	return new Promise((resolve, reject) => {
-		context.decodeAudioData(arrayBuffer, (buffer) => {
+		context.decodeAudioData(arrayBuffer, buffer => {
 			context = null;
 			resolve(buffer);
 		}, reject);
